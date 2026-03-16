464c8c90b13941ab9a923bf94aa732fdb72ecf05
getcararoundtn-coder <getcararoundtn@gmail.com>
2026-03-16 23:10:46 +0100

    Force IPv4 for Gmail SMTP

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -2,13 +2,23 @@ const nodemailer = require('nodemailer');
 
 const sendEmail = async (options) => {
   const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS
     },
     tls: {
-      rejectUnauthorized: false // يساعد في بيئات معينة
+      rejectUnauthorized: false,
+      ciphers: 'SSLv3'
+    },
+    connectionTimeout: 60000,
+    greetingTimeout: 30000,
+    socketTimeout: 60000,
+    // فرض استخدام IPv4
+    lookup: (hostname, options, callback) => {
+      require('dns').lookup(hostname, { family: 4 }, callback);
     }
   });
 
